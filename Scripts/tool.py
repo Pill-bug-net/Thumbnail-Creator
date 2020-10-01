2ba959bd8baede39cc0181aaaacbc068815592d6
Pill-bug <72141404+Pill-bug-net@users.noreply.github.com>
2020-10-02 08:47:38 +0900

    URLsupport

--- Scripts/tool.py
+++ Scripts/tool.py
@@ -2,10 +2,10 @@ from PIL import Image
 import tkinter as tk
 from tkinter import filedialog as fd
 
-def callback():
-    name= fd.askopenfilename()
-    targetDir = fd.askdirectory()
-    im = Image.open(name)
+import urllib
+
+def createThumbnail(filepath,targetDir):
+    im = Image.open(filepath)
     MAX_SIZE = (250,250)
     MID_SIZE = (125,125)
     SML_SIZE = (60,60)
@@ -17,10 +17,17 @@ def callback():
     
     im.thumbnail(SML_SIZE)
     im.save(targetDir + "\\SML_SIZE.png")
-    
-    #im.show()
 
-tk.Button(text='Chose image file',command=callback).pack(fill=tk.X)
+def createThumbnailBasedOnLocal():
+    name= fd.askopenfilename()
+    targetDir = fd.askdirectory()
+    createThumbnail(name,targetDir)
+
+
+def createThumbnailBasedOnOnline():
+    urllib.urlretrieve("http://www.gunnerkrigg.com//comics/00000001.jpg", "00000001.jpg")
+
+tk.Button(text='Chose image from local file',command=createThumbnailBasedOnLocal).pack(fill=tk.X)
+tk.Button(text='PastURL and creating thumbnail', command=createThumbnailBasedOnOnline).pack(fill=tk.X)
 tk.mainloop()
-#im = Image.open("C:\\Users\\yuta\\Pictures\\pill-bug.PNG")
 
